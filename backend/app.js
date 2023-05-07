176e6cae84beeab1d557817a3a5cd189eaba5dd7
sanuchakraborty <schakrabortty@iitg.ac.in>
2023-05-08 00:25:56 +0530

    some changes

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
 const errorMiddleware = require("./middleware/error");
-
+const cors = require('cors');
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
@@ -20,17 +20,17 @@ const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
 const payment = require("./routes/paymentRoute");
-
+app.use(cors());
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-app.use(express.static(path.join(__dirname , "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("*", (req, res) => {
   res.status(200).json({
-    message:'backend deployed'
-  })
+    message: "backend deployed",
+  });
   // res.sendFile(path.resolve(__dirname , "../frontend/build/index.html"));
 });
 //Middleware for error
